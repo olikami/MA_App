24e1113d65991f0c24d84a62fc0d8639c10f2c35
Oliver Kamer <oliver@kamer.ch>
2023-06-30 11:45:41 +0200

    dismiss keyboard and beautiful code

--- PeerChat/Views/Settings/SettingsView.swift
+++ PeerChat/Views/Settings/SettingsView.swift
@@ -11,33 +11,34 @@ struct SettingsView: View {
 
   @EnvironmentObject private var model: Model
   @State private var name: String = ""
-    
-    private var isNameChanged: Bool {
-        name != model.myPerson.name
-      }
+  @FocusState private var nameIsFocused: Bool
 
+  private var isNameChanged: Bool {
+    name != model.myPerson.name
+  }
 
   var body: some View {
     NavigationView {
       List {
         Section("Name") {
-            HStack{
-                TextField(
-                    "Name",
-                    text: $name
-                )
-                Button {
-                    model.setName(newName: name)
-                } label: {
-                  Image(systemName: "checkmark.circle.fill")
-                    .font(.system(size: 24, weight: .bold))
-                    .foregroundColor(.blue)
-                    .opacity(isNameChanged ? 1 : 0).animation(.spring(), value: name)
-                }
-                .frame(maxWidth: isNameChanged ? nil : 0)
-                .animation(.spring(), value: name)
-               
+          HStack {
+            TextField(
+              "Name",
+              text: $name
+            ).focused($nameIsFocused)
+            Button {
+              model.setName(newName: name)
+              nameIsFocused = false
+            } label: {
+              Image(systemName: "checkmark.circle.fill")
+                .font(.system(size: 24, weight: .bold))
+                .foregroundColor(.blue)
+                .opacity(isNameChanged ? 1 : 0).animation(.spring(), value: name)
             }
+            .frame(maxWidth: isNameChanged ? nil : 0)
+            .animation(.spring(), value: name)
+
+          }
         }
       }
     }.onAppear {
