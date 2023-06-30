c3a69138388e0a96df0cdd39e9610b85bde9b34f
Oliver Kamer <oliver@kamer.ch>
2023-06-30 11:40:37 +0200

    Manual confirmation of username

--- PeerChat/Views/Settings/SettingsView.swift
+++ PeerChat/Views/Settings/SettingsView.swift
@@ -11,17 +11,33 @@ struct SettingsView: View {
 
   @EnvironmentObject private var model: Model
   @State private var name: String = ""
+    
+    private var isNameChanged: Bool {
+        name != model.myPerson.name
+      }
+
 
   var body: some View {
     NavigationView {
       List {
         Section("Name") {
-          TextField(
-            "Name",
-            text: $name
-          ).onChange(of: name) { newValue in
-            model.setName(newName: newValue)
-          }
+            HStack{
+                TextField(
+                    "Name",
+                    text: $name
+                )
+                Button {
+                    model.setName(newName: name)
+                } label: {
+                  Image(systemName: "checkmark.circle.fill")
+                    .font(.system(size: 24, weight: .bold))
+                    .foregroundColor(.blue)
+                    .opacity(isNameChanged ? 1 : 0).animation(.spring(), value: name)
+                }
+                .frame(maxWidth: isNameChanged ? nil : 0)
+                .animation(.spring(), value: name)
+               
+            }
         }
       }
     }.onAppear {
