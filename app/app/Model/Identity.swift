b6215c403dfaadb6f27337571376378605044074
Oliver Kamer <oliver@kamer.ch>
2023-08-17 22:41:25 +0200

    use more robus but truncate

--- app/app/Model/Identity.swift
+++ app/app/Model/Identity.swift
@@ -39,19 +39,20 @@ class Identity: ObservableObject {
     }
 
     if let cfData = SecKeyCopyExternalRepresentation(key, nil) as Data? {
-      let hash = SHA1(data: cfData)
-      let fingerprint = hash?.map { String(format: "%02hhX", $0) }.joined()
+      let hash = SHA512(data: cfData)
+      let fullFingerprint = hash?.map { String(format: "%02hhX", $0) }.joined()
 
-      return fingerprint?.chunked(by: 4).joined(separator: " ")
+      let truncatedFingerprint = String(fullFingerprint?.prefix(24) ?? "")
+      return truncatedFingerprint.chunked(by: 4).joined(separator: " ")
     }
 
     return nil
   }
 
-  private func SHA1(data: Data) -> Data? {
-    var hash = [UInt8](repeating: 0, count: Int(CC_SHA1_DIGEST_LENGTH))
+  private func SHA512(data: Data) -> Data? {
+    var hash = [UInt8](repeating: 0, count: Int(CC_SHA512_DIGEST_LENGTH))
     data.withUnsafeBytes {
-      _ = CC_SHA1($0.baseAddress, CC_LONG(data.count), &hash)
+      _ = CC_SHA512($0.baseAddress, CC_LONG(data.count), &hash)
     }
     return Data(hash)
   }
