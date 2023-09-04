fbe21d4d507ff3d2332edb452d5526689361880a
Oliver Kamer <oliver@kamer.ch>
2023-09-04 16:39:45 +0200

    remove author from admin

--- backend/messaging/admin.py
+++ backend/messaging/admin.py
@@ -8,10 +8,7 @@ admin.site.register(Location)
 class MessageAdmin(admin.ModelAdmin):
     list_display = ("content", "sent", "location")
     list_filter = ("sent", "location")
-    search_fields = (
-        "content",
-        "author__username",
-    )  # assuming `ApplicationUser` has a field named `username`
+    search_fields = ("content",)
 
 
 admin.site.register(Message, MessageAdmin)
