758056562fb58a9328a6488587f292cd99e0a2c8
Oliver Kamer <oliver@kamer.ch>
2023-09-04 21:19:30 +0200

    Display message info

--- App/app/Views/Chat/MessageRow/ChatMessageRow.swift
+++ App/app/Views/Chat/MessageRow/ChatMessageRow.swift
@@ -5,6 +5,8 @@
 //  Created by Oliver Kamer on 5/20/23.
 //
 
+import Security
+import SwiftASN1
 import SwiftUI
 import X509
 
@@ -13,6 +15,8 @@ struct ChatMessageRow: View {
   let message: Message
   let geo: GeometryProxy
   var isCurrentUser: Bool
+
+  @State var showMessageInfo: Bool = false
 
   var userCertificate: Certificate? {
     guard let certificate_string = message.certificate else {
@@ -66,9 +70,85 @@ struct ChatMessageRow: View {
             .padding()
         }
         Text(formatDateCompact(date: message.sent)).foregroundColor(Color.gray).font(.footnote)
-      }
+      }.contentShape(Rectangle())
+        .onTapGesture {
+          showMessageInfo = true
+        }
       if !isCurrentUser {
         Spacer(minLength: geo.size.width * 0.2)
+      }
+    }.sheet(isPresented: $showMessageInfo) {
+      ChatMessageInfoView(message: message, isCurrentUser: isCurrentUser)
+    }
+  }
+}
+
+struct ChatMessageInfoView: View {
+
+  let message: Message
+  let isCurrentUser: Bool
+  @State private var signatureString: String = "Signature verification failed"
+
+  var body: some View {
+    VStack(alignment: .leading) {
+      GeometryReader { geometry in
+        ChatMessageRow(message: message, geo: geometry, isCurrentUser: isCurrentUser)
+      }
+
+      Spacer()
+      if let certificatesString = message.certificate {
+        let certificates = getCertificatesFromCertifcateString(
+          certificatestring: certificatesString)
+        let author = getCommonName(subject: certificates[0].subject)
+        let role = String(
+          getCommonName(subject: certificates[0].issuer).split(separator: " ").first ?? "")
+
+        Text("Author: \(author)")
+        Text("Timestamp: \(formatDateCompact(date: message.sent))")
+        Text("Signature: \(signatureString)")
+        Text("Role: \(role)")
+
+        Spacer()
+        HStack {
+          Spacer()
+          CertificateView(certificates: certificates)
+          Spacer()
+        }
+
+      }
+      Spacer()
+    }
+    .onAppear(perform: verifySignature)
+    .padding()
+  }
+
+  func verifySignature() {
+    if let certificatesString = message.certificate {
+      let certificates = getCertificatesFromCertifcateString(certificatestring: certificatesString)
+
+      do {
+        var serializer = DER.Serializer()
+        try serializer.serialize(certificates[0])
+        let serializerData = Data(serializer.serializedBytes)
+        let secCertificate = SecCertificateCreateWithData(nil, serializerData as CFData)
+        guard let pubKey = SecCertificateCopyKey(secCertificate!) else { return }
+        if let signatureData = Data(base64Encoded: message.signature!) {
+          let messageData = message.content.data(using: .utf8)
+
+          var error: Unmanaged<CFError>?
+          let signatureVerified = SecKeyVerifySignature(
+            pubKey,
+            .rsaSignatureDigestPKCS1v15SHA512,
+            messageData! as CFData,
+            signatureData as CFData,
+            &error)
+
+          if signatureVerified {
+            signatureString = "Signature successfully verified.)"
+          }
+        }
+      } catch let error {
+        print(error)
       }
     }
   }
