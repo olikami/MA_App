cbf6e3492712267e7d54af6a4bf6031db11ed09a
Oliver Kamer <oliver@kamer.ch>
2023-09-26 11:49:51 +0200

    remove all capital App

--- App/app/Extensions/ISO8601DateFormatter+Exts.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-extension ISO8601DateFormatter {
-  static let shared: ISO8601DateFormatter = {
-    let formatter = ISO8601DateFormatter()
-    formatter.formatOptions = [.withInternetDateTime, .withFractionalSeconds]
-    return formatter
-  }()
-}
